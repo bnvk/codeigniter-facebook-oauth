4ee066e25b4c1bf300658771a642190981705059
Edward Hotchkiss <edward@edwardhotchkiss.com>
2011-03-29 23:54:51 -0700

    config.php base url fix

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'https://apps.edwardhotchkiss.com/posseup/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
